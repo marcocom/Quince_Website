94ee9f3585b6aac0ed7376e50aff90e41ad6d8df
Redo Aoura <radouane@quince.nl>
2014-04-16 11:59:59 +0200

    add autocomplete to customer and author fields

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -5,6 +5,6 @@
     <title>Items Page</title>
     <link href="css/reset.css" rel="stylesheet" />
     <link href="css/style.css" rel="stylesheet" />
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
 </head>
 <body>
